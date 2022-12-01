cdf7d135aeb603ecebe6e9bc8f1e4db64b345884
l.gogol <l.gogol@nakusi.games>
2022-12-01 23:37:43 +0300

    DONE

---
+++ Assets/_LevGogol/Scripts/YandexLocalization.cs
@@ -0,0 +1,59 @@
+using System;
+using TMPro;
+using UnityEngine;
+using YG;
+
+public class YandexLocalization : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUILocalization[] _textMeshProUGUILocalization;
+     
+     private string _language;
+     
+     private void OnEnable()
+     {
+         Translate();
+         YandexGame.GetDataEvent += Translate;
+     }
+
+     private void Translate()
+     {
+         if(_language == "")
+             _language = YandexGame.EnvironmentData.language;
+         
+         if(_language == "ru")
+             TranslateToRussia();
+     }
+
+     private void TranslateToRussia()
+     {
+         foreach (var localization in _textMeshProUGUILocalization)
+        {
+            localization.textMeshProUGUI.text = localization.ru;
+            localization.textMeshProUGUI.font = localization.ruFont;
+        }
+    }
+
+    private void TranslateToEnglish()
+    {
+        foreach (var localization in _textMeshProUGUILocalization)
+        {
+            localization.textMeshProUGUI.text = localization.en;
+            localization.textMeshProUGUI.font = localization.enFont;
+        }
+    }
+
+    private void OnDisable()
+    {
+        YandexGame.GetDataEvent -= Translate;
+    }
+}
+
+[Serializable]
+public struct TextMeshProUGUILocalization
+{
+    public TextMeshProUGUI textMeshProUGUI;
+    public string ru;
+    public TMP_FontAsset ruFont;
+    public string en;
+    public TMP_FontAsset enFont;
+}
